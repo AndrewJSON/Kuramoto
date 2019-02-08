f99561e29a572107d31a93130905692c59a79669
Andrew Jason Bishop <Andrew.Jason.Bishop@gmx.net>
2019-02-08 18:08:53 +0100

    csv writer as class

--- CSV_handler.py
+++ CSV_handler.py
@@ -12,23 +12,38 @@
 import csv
 import numpy as np
 
-a = np.array([0.3, 1.2, 6.8])
-b = np.array([0.4, 1.3, 6.9])
 
-phase_vectors = np.array( [[0.4, 1.3, 6.9], [0.3, 1.2, 6.8]] )
+class CSV_handler:
+
+    def __init__(self, _fileName):
+        self.fileName = _fileName
+
+
+    def writeVectorsToFile(self, _vectors):
+
+        with open( self.fileName, mode='w' ) as out_file:
+            csv_writer = csv.writer( out_file,      \
+                                     delimiter=',', \
+                                     quotechar='"', \
+                                     quoting=csv.QUOTE_MINIMAL )
+
+            self.writeOneRowPerVector( _vectors, csv_writer )
+
+
+    def writeOneRowPerVector(self, _vectors, _writer):
+
+            for vector in _vectors:
+                _writer.writerow( vector )
 
 
 if __name__ == '__main__':
 
-    with open('employee_file.csv', mode='w') as employee_file:
-        employee_writer = csv.writer( employee_file,\
-                                      delimiter=',',\
-                                      quotechar='"',\
-                                      quoting=csv.QUOTE_MINIMAL )
+    phase_vectors = np.array( [[0.4, 1.3, 6.9], [0.3, 1.2, 6.8], [0.2, 1.1, 6.7]] )
+    myCSV_handler = CSV_handler('test_file.csv')
 
-        for phase_vector in phase_vectors:
-            employee_writer.writerow( phase_vector )
+    myCSV_handler.writeVectorsToFile( phase_vectors )
 
+    print("done.")
 
 ''' END '''
 
